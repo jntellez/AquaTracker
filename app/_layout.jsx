5838b206e8566b9d6ac490c369f952ff925564c8
jntellez <juantellez916@gmail.com>
2024-12-04 23:30:23 -0600

    swipeable card

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,58 +1,51 @@
-import React, { useEffect, useState } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import React from "react";
 import { Stack } from "expo-router";
-import { WaterConsumptionProvider } from "../store/WaterConsumptionStore"; // Importar el provider
+import { WaterConsumptionProvider } from "../store/WaterConsumptionStore";
 import Logo from "../assets/Logo";
 import theme from "../styles/theme";
-import { Ionicons } from "@expo/vector-icons"; // Importamos Ionicons para usar los iconos
+import { Ionicons } from "@expo/vector-icons";
 import { TouchableOpacity } from "react-native";
-import { useRouter, usePathname } from "expo-router"; // Usamos el hook usePathname
+import { useRouter, usePathname } from "expo-router";
 
 export default function Layout() {
   const { colors } = theme;
-  const router = useRouter(); // Usamos el hook de router
-  const pathname = usePathname(); // Usamos usePathname para obtener la ruta actual
-  const [isHomePage, setIsHomePage] = useState(false); // Estado para manejar si estamos en la página de inicio
-
-  useEffect(() => {
-    // Verificamos si estamos en la página de inicio
-    if (pathname === "/") {
-      setIsHomePage(true);
-    } else {
-      setIsHomePage(false);
-    }
-  }, [pathname]); // Este useEffect se ejecuta cada vez que cambia la ruta
+  const router = useRouter();
+  const pathname = usePathname();
+  const isHomePage = pathname === "/";
 
   const handlePress = () => {
-    // Navegamos a la ruta '/profile'
     router.navigate("/profile");
   };
 
   return (
-    <WaterConsumptionProvider>
-      <Stack
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: colors.primary,
-          },
-          headerTitleStyle: {
-            fontWeight: "bold", // Estilo del título
-          },
-          headerTitle: () => <Logo style={{ paddingBottom: 10 }} />,
-          headerRight: () =>
-            !isHomePage && ( // Si no estamos en la página de inicio (ruta '/')
-              <TouchableOpacity
-                onPress={handlePress}
-                style={{ marginRight: 10 }}
-              >
-                <Ionicons
-                  name="person-circle-outline" // Icono circular de usuario
-                  size={38}
-                  color={colors.border} // El color puede ser el que prefieras
-                />
-              </TouchableOpacity>
-            ),
-        }}
-      />
-    </WaterConsumptionProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <WaterConsumptionProvider>
+        <Stack
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: colors.primary,
+            },
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+            headerTitle: () => <Logo style={{ paddingBottom: 10 }} />,
+            headerRight: () =>
+              !isHomePage && (
+                <TouchableOpacity
+                  onPress={handlePress}
+                  style={{ marginRight: 10 }}
+                >
+                  <Ionicons
+                    name="person-circle-outline"
+                    size={38}
+                    color={colors.border}
+                  />
+                </TouchableOpacity>
+              ),
+          }}
+        />
+      </WaterConsumptionProvider>
+    </GestureHandlerRootView>
   );
 }
--- components/log/ActivityCard.jsx
+++ components/log/ActivityCard.jsx
@@ -1,21 +1,54 @@
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import ReanimatedSwipeable from "react-native-gesture-handler/ReanimatedSwipeable";
 import theme from "../../styles/theme";
 
 export default function ActivityCard({ data }) {
+  const [isSwiped, setIsSwiped] = useState(false);
+
   const time = new Date(data.time).toLocaleTimeString("es-MX", {
     hour: "numeric",
     minute: "2-digit",
     hour12: true,
   });
 
+  const handleDelete = () => {
+    console.log("Activity deleted");
+    setIsSwiped(false);
+  };
+
+  const renderRightActions = () => (
+    <TouchableOpacity
+      onPress={() => handleDelete()}
+      style={styles.deleteButton}
+    >
+      <Text style={styles.deleteText}>Eliminar</Text>
+    </TouchableOpacity>
+  );
+
+  const handleSwipeOpen = () => {
+    setIsSwiped(true);
+  };
+
+  const handleSwipeClose = () => {
+    setIsSwiped(false);
+  };
+
   return (
-    <View style={styles.activityCard}>
-      <View>
-        <Text style={styles.activityName}>{data.name}</Text>
-        <Text style={styles.activityTime}>{time}</Text>
+    <ReanimatedSwipeable
+      renderRightActions={renderRightActions}
+      overshootRight={true} // efecto de rebote hacia la derecha
+      onSwipeableOpen={handleSwipeOpen} // Detecta cuando se desliza
+      onSwipeableClose={handleSwipeClose} // Detecta cuando se cierra
+    >
+      <View style={[styles.activityCard, isSwiped && styles.noRightRadius]}>
+        <View>
+          <Text style={styles.activityName}>{data.name}</Text>
+          <Text style={styles.activityTime}>{time}</Text>
+        </View>
+        <Text style={styles.activityAmount}>{data.amount}</Text>
       </View>
-      <Text style={styles.activityAmount}>{data.amount}</Text>
-    </View>
+    </ReanimatedSwipeable>
   );
 }
 
@@ -28,6 +61,10 @@ const styles = StyleSheet.create({
     padding: theme.spacing.medium,
     borderRadius: theme.borderRadius.small,
     marginBottom: theme.spacing.small,
+  },
+  noRightRadius: {
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0,
   },
   activityName: {
     ...theme.typography.body,
@@ -42,5 +79,20 @@ const styles = StyleSheet.create({
     ...theme.typography.body,
     fontWeight: "bold",
     color: theme.colors.textPrimary,
+  },
+  deleteButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: theme.colors.accent,
+    width: 80,
+    borderRadius: theme.borderRadius.small,
+    marginBottom: theme.spacing.small,
+    borderTopLeftRadius: 0,
+    borderBottomLeftRadius: 0,
+  },
+  deleteText: {
+    color: theme.colors.buttonText,
+    fontSize: theme.typography.body.fontSize,
+    fontWeight: "bold",
   },
 });
